8f83a7a566246ad8150c2a8937b5609cdeb7e0d6
OkJoe <ilestcree@gmail.com>
2024-12-13 20:58:12 -0500

    20241213 update new version of main.py

--- main.py
+++ main.py
@@ -1,99 +1,157 @@
-import subprocess
 import sys
+import subprocess
 from fontTools import ttLib
 import copy
-import chinese_converter as cvt # used only for morphs
 
-def read_dict(file_name,delim=None):
-    d = dict()
-    with open(file_name,'r',encoding='utf8') as f: lines = f.read().splitlines()
+class Char:
+    def __init__(self, name, type = None, parent_name = None, parent_type = None):
+        self.name = name
+        if len(name) == 1: self.ord = ord(name)
+        else: self.ord = None # char is dummy, unicode unavailable 
+        if type is None:
+            if len(name) == 1: self.type = 'reg' 
+            else: self.type = 'dummy'
+        else: self.type = type # 'reg' 'dummy' 'reduced'
+        self.parent_name = parent_name
+        self.parent_type = parent_type # 'phonetic' 'dummy' 'reduced' 'alternative'
+
+    def set_parent(self, parent_name, parent_type):
+        self.parent_name = parent_name
+        self.parent_type = parent_type
+
+    def find_substitution_glyph(self, forest, displayed_chars, not_displayed_chars, display_subs, get_glyph_code, missing_glyphs, is_BMP):
+        # print('processing ' + self.name)
+        if self.parent_name == None or self.parent_name in not_displayed_chars or self.name in displayed_chars: 
+            return self.get_glyph(display_subs.get(self.name,[]), get_glyph_code, missing_glyphs, is_BMP)
+        else: 
+            parent_result = forest.dict[self.parent_name].find_substitution_glyph(forest, displayed_chars, not_displayed_chars, display_subs, get_glyph_code, missing_glyphs, is_BMP)
+            if parent_result is None: return self.get_glyph(display_subs.get(self.name,[]), get_glyph_code, missing_glyphs, is_BMP)
+            else: return parent_result
+    
+    def get_glyph(self, sub_list, get_glyph_code, missing_glyphs, is_BMP):
+        if self.type != 'reg' and len(sub_list) == 0: raise Exception("Missing Char Substitution Suggestions: " + self.name)
+        if self.type == 'reg': add_to_sub_list = [self.name]
+        else: add_to_sub_list = []
+        for char_name in [*sub_list, *add_to_sub_list]:
+            if len(char_name) > 1: raise Exception("Invalid Substitution Char: " + char_name)
+            glyph_code = get_glyph_code(ord(char_name))
+            if glyph_code is None: 
+                if is_BMP is False or ord(char_name) < 16 ** 4: missing_glyphs.add(char_name)
+            else: return char_name, glyph_code
+        return None
+
+class Forest:
+    def __init__(self):
+        self.dict = {None:None}
+    
+    def add(self, char, dup_chars): 
+        if char.name in self.dict: 
+            if dup_chars is not None: dup_chars.add(char.name)
+        else: self.dict[char.name] = char
+
+def parse(parent_name, children_string, forest, dup_chars, parent_type = 'phonetic', mode = None): # parent must be in forest, only children are added to the forest in this method 
+    if parent_name not in forest.dict: raise Exception("Parent Missing: " + parent_name)
+    if (children_string.count('(') != children_string.count(')') 
+        or children_string.count('[') != children_string.count(']') 
+        or children_string.count('{') != children_string.count('}')): 
+        raise Exception("Format Error: " + children_string)
+    if '\t' in children_string: parse(parent_name, children_string.replace('\t', '(', 1) + ')', forest, dup_chars, parent_type = parent_type, mode = mode); return
+    nested_string = ''
+    nesting = 0
+    previous_char = parent_name
+    for cursor in children_string:
+        current_char = cursor
+        if cursor in '([{': nesting += 1
+        elif cursor in '}])': nesting -= 1
+        if nesting == 0:
+            if cursor == ']': current_char = nested_string + ']'
+            if cursor == '}': parse(previous_char, nested_string[1:], forest, dup_chars, parent_type = 'alternative', mode = mode)
+            elif cursor == ')': parse(previous_char, nested_string[1:], forest, dup_chars, parent_type = 'phonetic', mode = mode)
+            elif current_char != parent_name: 
+                if mode == 'init': forest.add(Char(current_char, parent_name = parent_name, parent_type = parent_type), dup_chars = dup_chars)
+                if mode == 'mod' and parent_name is not None: forest.dict[current_char].set_parent(parent_name, parent_type)
+                previous_char = current_char
+            if cursor in '}])': nested_string = ''
+        if nesting > 0: nested_string += cursor
+
+def read_dict(file_name, forest, dup_chars, mode): # mode can be 'init' or 'mod'
+    with open(file_name,'r',encoding='utf8') as file: lines = file.read().splitlines()
     for line in lines:
         if line[0] == '#': continue
-        if delim is None: d[line[0]] = line.split('\t')[0][1:] # [key][val][val]...\t[comment]
-        else: # [key][key]...[delim][val][val]...\t[comment]
-            for key in line.split(delim)[0].split('#')[0]:
-                d[key] = line.split(delim)[1].split('\t')[0]
-    return d # {'key':'val'+'val'+..., 'another key':'...', ...}
+        parse(None, line.split('\t#')[0], forest, dup_chars, mode = mode)
 
-def mod_dict_keys(d,key_dict):
-    for key_from,key_to in key_dict.items():
-        chars_in_key_from = d.pop(key_from)
-        d.setdefault(key_to,[]).extend(chars_in_key_from)
-    return True
-
-def read_morphs(file_name,pre_assigned_dict): # traditional morphs
-    morph_dict = dict()
-    with open(file_name,'r',encoding='utf8') as f: lines = f.read().splitlines()
+def read_subs(file_name):
+    subs = dict()
+    with open(file_name,'r',encoding='utf8') as file: lines = file.read().splitlines()
     for line in lines:
         if line[0] == '#': continue
-        morph, chars = line.split('\t')[0], [*line.split('\t')[1]]
-        morph_dict[morph] = chars
-        for char in chars.copy():
-            if char in pre_assigned_dict: chars.remove(char)
-        if len(morph_dict[morph]) == 0: morph_dict.pop(morph)
-    for char, morph in pre_assigned_dict.items():
-        morph_dict.setdefault(morph,[]).append(char)
-    return morph_dict
+        l = line.split('\t#')[0].split('\t')
+        subs[l[0]] = l[1:]
+    return subs
 
 # Main
-# 1. read chars with assigned morphs
-# 2. read morphs and then modify with pre-assigned chars
-# 3. replace unwanted morphs then merge secondary morphs
-# 4. modify the font file and sub with desired displayed form
-# 5. save font and delete excessive glyphs
-font_dir = 'SourceHan/'; src_dir = 'src(traditional)/'; log_dir = 'log/'
+font_dir = 'SourceHan/'; src_dir = 'src/'; log_dir = 'log/'
 
-pre_assigned_dict = read_dict(src_dir + 'exception_chars.txt')
-morph_dict = read_morphs('phonographeme_dict.txt',pre_assigned_dict)
+font_path = font_dir + 'SourceHanSans-Regular.ttf' 
+font_output = font_dir + 'SourceHanSansPhonetic-Regular.ttf' 
 
-font_path = font_dir + 'SourceHanSansStr-Regular.ttf' # SourceHanSansStr-Regular.otf
-font_output = font_dir + 'SourceHanSans-Phonetic.ttf' # 
+forest =  Forest()
+dup_chars = set()
+read_dict(src_dir + 'phonograph_dict.txt', forest, dup_chars, mode = 'init')
+read_dict(src_dir + 'phonograph_hierarchy.txt', forest, dup_chars, mode = 'mod')
+read_dict(src_dir + 'phonograph_relation_mod.txt', forest, dup_chars, mode = 'mod')
+displayed_chars = Forest()
+read_dict(src_dir + 'phonograph_displayed.txt', displayed_chars, None, mode = 'init')
+not_displayed_chars = Forest()
+read_dict(src_dir + 'phonograph_not_displayed.txt', not_displayed_chars, None, mode = 'init')
+subs = read_subs(src_dir + 'phonograph_display_subs.txt')
 
 font = ttLib.TTFont(font_path)
 
-mod_dict_keys(morph_dict,read_dict(src_dir + 'rare_PhGs.txt'))
-
-morph_sub_dict = read_dict(src_dir + 'display_mod(custom).txt',delim='\t')
-
-# convert morph to simplified
-# for morph in morph_dict.copy():
-#     if cvt.to_simplified(morph) not in morph_dict:
-#         morph_dict[cvt.to_simplified(morph)] = morph_dict.pop(morph)
-#         if morph in morph_sub_dict: morph_sub_dict[cvt.to_simplified(morph)] = morph_sub_dict.pop(morph)
-
-# fonttools cannot handle ord(char) >= 16**4
-for morph, chars in morph_dict.items():
-    for char in chars:
-        if ord(char) >= 16**4: chars.remove(char); print('char ' + char + ' overflow and deleted')
-
-glyphs_to_include = set(font.getGlyphOrder()); glyphs_morphs = set(); missing_morph = set()
-all_chars = set(); all_morph_glyphs = set()
+all_accounted_chars = set(); all_displayed_chars = set(); missing_glyphs = set()
+# glyph_labels_to_include = set(font.getGlyphOrder()); glyph_labels_sub_to = set()
 for cmap,ori_cmap in zip(font['cmap'].tables,copy.deepcopy(font['cmap'].tables)):
-    if not ((cmap.platformID,cmap.platEncID) == (0,4) or (cmap.platformID,cmap.platEncID) == (3,10)): continue
-    for morph, chars in morph_dict.items():
-        ord_morph = ord(morph)
-        for sub_morph in morph_sub_dict.get(morph,[]): # sub can have multiple choices
-            if ord(sub_morph) in ori_cmap.cmap: ord_morph = ord(sub_morph); break
-        if ord_morph not in ori_cmap.cmap: missing_morph.add(chr(ord_morph)); continue
-        glyphs_morphs.add(ori_cmap.cmap[ord_morph])
-        for char in chars:
-            glyphs_to_include.discard(ori_cmap.cmap.get(ord(char)))
-            cmap.cmap[ord(char)] = ori_cmap.cmap[ord_morph]
-            all_chars.add(char); all_morph_glyphs.add(chr(ord_morph))
-glyphs_to_include.update(glyphs_morphs)
+    if (cmap.platformID,cmap.platEncID) in [(0,3), (3,1)]: is_BMP = True
+    elif (cmap.platformID,cmap.platEncID) in [(0,4), (0,6), (3,10)]: is_BMP = False
+    else: continue
+    for char_ord in ori_cmap.cmap:
+        char = chr(char_ord)
+        if char not in forest.dict: continue
+        if char is None or forest.dict[char].type != 'reg': continue
+        map_to_char_and_glyph = forest.dict[char].find_substitution_glyph(forest, displayed_chars.dict, not_displayed_chars.dict, subs, ori_cmap.cmap.get, missing_glyphs, is_BMP)
+        if map_to_char_and_glyph is None: continue
+        map_to_char, cmap.cmap[char_ord] = map_to_char_and_glyph
+        all_accounted_chars.add(char); all_displayed_chars.add(map_to_char)
+#            glyph_labels_to_include.discard(ori_cmap.cmap[char_ord]); glyph_labels_sub_to.add(cmap.cmap[char_ord])
+# glyph_labels_to_include.update(glyph_labels_sub_to)
 
-glyphs_to_include_file_name = log_dir + 'glyphs_to_include.txt'
-with open(glyphs_to_include_file_name,"w",encoding="utf8") as f:
-    f.write(','.join(sorted(glyphs_to_include)))
+"""
+glyph_labels_to_include2 = set()
+for cmap in font['cmap'].tables:
+    for char_ord in cmap.cmap:
+        glyph_labels_to_include2.add(cmap.cmap[char_ord])
 
-with open(log_dir + 'font_missing_phonographemes.txt',"w",encoding="utf8") as f:
-    f.write(''.join(sorted(missing_morph)))
+glyph_labels_to_include.update(glyph_labels_to_include2)
+"""
+
+with open(log_dir + 'missing_glyphs.txt',"w",encoding="utf8") as f:
+    f.write('\n'.join(sorted(missing_glyphs)))
 with open(log_dir + 'chars_covered.txt',"w",encoding="utf8") as f:
-    f.write(''.join(sorted(all_chars)))
-with open(log_dir + 'phonographemes.txt',"w",encoding="utf8") as f:
-    f.write(''.join(sorted(all_morph_glyphs)))
+    f.write('\n'.join(sorted(all_accounted_chars)))
+with open(log_dir + 'chars_displayed.txt',"w",encoding="utf8") as f:
+    f.write('\n'.join(sorted(all_displayed_chars)))
 
 font.save(font_output); print('font saved, deleting excessive glyphs')
+
+font = ttLib.TTFont(font_output)
+glyph_labels_to_include = set()
+for cmap in font['cmap'].tables:
+    for char_ord in cmap.cmap:
+        glyph_labels_to_include.add(cmap.cmap[char_ord])
+
+glyphs_to_include_file_name = log_dir + 'glyph_labels_to_include.txt'
+with open(glyphs_to_include_file_name,"w",encoding="utf8") as f:
+    f.write(','.join(sorted(glyph_labels_to_include)))
 
 subprocess.run('pyftsubset ' + font_output + ' --output-file=' + font_output + ' --glyphs-file=' + glyphs_to_include_file_name, shell=True) # may not work, try changing the format of the input file 
 print('done')
---
+++ src(old)/src(minimalist)/SourceHan/README.md
@@ -0,0 +1,3 @@
+The fonts in this sub-directory are or are modified from [source-han-sans](https://github.com/adobe-fonts/source-han-sans), which is a product by adobe-fonts. 
+
+The Regular font files are sample input font files. The [main](../main.py) program convert a font files into a Phonetic font file. 
