007f4825cada2790699d363e2734993e185f110a
OkJoe <ilestcree@gmail.com>
2025-01-09 19:54:28 -0500

    update algorithm

--- mainttfotf.py
+++ mainttfotf.py
@@ -8,8 +8,10 @@ from defcon import Font
 from ufo2ft import compileTTF
 from ufo2ft import compileOTF
 
-# this script processes sources files in ufo 
-# to process ttf/otf, see mainttfotf.py 
+# in this script 
+# the main algorithm is performed on ttf file 
+# it is more time consuming if the font is compiled from ufo 
+# but more direct if the font is provided directly in ttf 
 
 class Char:
     def __init__(self, name, type = None, parent_name = None, parent_type = None):
@@ -28,7 +30,6 @@ class Char:
         self.parent_type = parent_type
 
     def find_substitution_glyph(self, forest, displayed_chars, not_displayed_chars, display_subs, get_glyph_code, missing_glyphs, is_BMP):
-        # get glyph code takes a char's ord as input and returns the glyph's name 
         # print('processing ' + self.name)
         if self.parent_name == None or self.parent_name in not_displayed_chars or self.name in displayed_chars: 
             return self.get_glyph(display_subs.get(self.name,[]), get_glyph_code, missing_glyphs, is_BMP)
@@ -44,13 +45,9 @@ class Char:
         for char_name in [*sub_list, *add_to_sub_list]:
             if len(char_name) > 1: raise Exception("Invalid Substitution Char: " + char_name)
             glyph_code = get_glyph_code(ord(char_name))
-            # print(char_name,ord_to_hex(ord(char_name)),glyph_code)
-            # sys.exit(glyph_code)
             if glyph_code is None: 
                 if is_BMP is False or ord(char_name) < 16 ** 4: missing_glyphs.add(char_name)
-            else: 
-                # sys.exit(char_name)
-                return char_name, glyph_code
+            else: return char_name, glyph_code
         return None
 
 class Forest:
@@ -110,8 +107,6 @@ font_sup_path = font_dir + 'SourceHanSansSup-Regular.ufo'
 font_combined_path = font_dir + 'temp.ufo' 
 font_output = font_dir + 'SourceHanSansPhonetic-Regular.ttf' 
 
-print('compiling phonograph-character hierarchy') 
-
 forest =  Forest()
 dup_chars = set()
 read_dict(src_dir + 'phonograph_dict.txt', forest, dup_chars, mode = 'init')
@@ -123,72 +118,68 @@ not_displayed_chars = Forest()
 read_dict(src_dir + 'phonograph_not_displayed.txt', not_displayed_chars, None, mode = 'init')
 subs = read_subs(src_dir + 'phonograph_display_subs.txt')
 
-print('processing ufo files')
+# the following part compiles the ufo's into ttf 
+# if one needs to supplement extra glyphs to the original source file, both files has to be in ufo format 
+# it shall be commented out if the source file is ttf/otf 
+# '''
+shutil.copytree(font_path, font_combined_path, dirs_exist_ok = True)
+source_dir = font_sup_path + "/glyphs"
+destination_dir = font_combined_path + "/glyphs"
 
-ufo = Font(font_path)
-ufo_sup = Font(font_sup_path)
-for glyph in ufo_sup:
-    if glyph.name in ufo:
-        print("warning: supplied glyph '" + glyph.name + "' already exists in the source font")
-        for unicode in ufo[glyph.name].unicodes:
-            if unicode not in glyph.unicodes: glyph.unicodes.append(unicode)
-    ufo.insertGlyph(glyph, name=None)
+# Iterate over all files in the source directory
+for filename in os.listdir(source_dir):
+    if filename == 'contents.plist': continue
+    source_file = os.path.join(source_dir, filename)
 
-'''
-print('copying unicode data')
-unicodeData_copy = copy.deepcopy(ufo.unicodeData)
-'''
+    # Check if the item is a file (not a directory)
+    if os.path.isfile(source_file):
+        destination_file = os.path.join(destination_dir, filename)
+        shutil.copy(source_file, destination_file)
 
-print('substituting glyph maps')
+with (open(font_combined_path + "/glyphs/contents.plist","r",encoding="utf8") as f, 
+      open(font_sup_path + "/glyphs/contents.plist","r",encoding="utf8") as g):
+    combined_contents = f.read().partition('  </dict>')[0] + g.read().partition('  <dict>\n')[2]
+with open(font_combined_path + "/glyphs/contents.plist","w",encoding="utf8") as f:
+    f.write(combined_contents)
+
+ufo = Font(font_combined_path)
+font = compileTTF(ufo)
+# ''' 
+# the part above compiles the ufo's into ttf 
+
+# the following line reads ttf/otf source file directly 
+# this line shall be used only when the source file is ttf/otf 
+# however, one cannot supply supplementary glyphs through this method 
+# font = ttLib.TTFont(font_path)
 
 all_accounted_chars = set(); all_displayed_chars = set(); missing_glyphs = set()
+# glyph_labels_to_include = set(font.getGlyphOrder()); glyph_labels_sub_to = set()
+best_cmap = font.getBestCmap()
+for cmap,ori_cmap in zip(font['cmap'].tables,copy.deepcopy(font['cmap'].tables)):
+    if (cmap.platformID,cmap.platEncID) in [(0,3), (3,1)]: is_BMP = True
+    elif (cmap.platformID,cmap.platEncID) in [(0,4), (0,6), (3,10)]: is_BMP = False
+    else: continue
+    # for char_ord in ori_cmap.cmap:
+    #     char = chr(char_ord)
+    #     if char not in forest.dict: continue
+    for char in forest.dict:
+        if char is None or forest.dict[char].type != 'reg': continue
+        if is_BMP and ord(char) > 65535: continue
+        map_to_char_and_glyph = forest.dict[char].find_substitution_glyph(forest, displayed_chars.dict, not_displayed_chars.dict, subs, best_cmap.get, missing_glyphs, False) # is_BMP)
+        if map_to_char_and_glyph is None: continue
+        map_to_char, cmap.cmap[ord(char)] = map_to_char_and_glyph
+        all_accounted_chars.add(char); all_displayed_chars.add(map_to_char)
+#            glyph_labels_to_include.discard(ori_cmap.cmap[char_ord]); glyph_labels_sub_to.add(cmap.cmap[char_ord])
+# glyph_labels_to_include.update(glyph_labels_sub_to)
 
-def ord_to_hex(i):
-    if i < 16 ** 4: return format(i, '04x').upper()
-    else: return format(i, '05x').upper()
+"""
+glyph_labels_to_include2 = set()
+for cmap in font['cmap'].tables:
+    for char_ord in cmap.cmap:
+        glyph_labels_to_include2.add(cmap.cmap[char_ord])
 
-'''
-def find_glyph_name_from_char_ord(i):
-    return ufo.unicodeData.glyphNameForUnicode(ord_to_hex(i))
-
-
-print(ufo.unicodeData.glyphNameForUnicode(ord('a')))
-sys.exit()
-'''
-
-for char in forest.dict:
-    if char is None or forest.dict[char].type != 'reg': continue
-    map_to_char_and_glyph = forest.dict[char].find_substitution_glyph(forest, displayed_chars.dict, not_displayed_chars.dict, subs, ufo.unicodeData.glyphNameForUnicode, missing_glyphs, False) # is_BMP)
-    if map_to_char_and_glyph is None: continue
-    map_to_char, map_to_glyph_name = map_to_char_and_glyph
-    original_glyph_name = ufo.unicodeData.glyphNameForUnicode(ord(char))
-    if original_glyph_name is not None and map_to_glyph_name != original_glyph_name:
-        original_glyph_name_unicodes = ufo[original_glyph_name].unicodes
-        original_glyph_name_unicodes.remove(ord(char))
-        ufo[original_glyph_name].unicodes = original_glyph_name_unicodes
-        map_to_glyph_name_unicodes = ufo[map_to_glyph_name].unicodes
-        map_to_glyph_name_unicodes.append(ord(char))
-        ufo[map_to_glyph_name].unicodes = map_to_glyph_name_unicodes
-        '''
-        print(type(ufo[map_to_glyph_name].unicodes))
-        print(ord(char))
-        print(original_glyph_name,map_to_glyph_name)
-        print(int(original_glyph_name[3:],16),int(map_to_glyph_name[3:],16))
-        print(ufo[original_glyph_name].unicodes)
-        print(ufo[map_to_glyph_name].unicodes)
-        sys.exit()
-        '''
-    all_accounted_chars.add(char); all_displayed_chars.add(map_to_char)
-
-print('deleting unused glyphs')
-with open(font_path + '/features.fea',"r",encoding="utf8") as f: features = f.read()
-glyphs_to_delete = set()
-for glyph in ufo:
-    if len(glyph.unicodes) == 0 and glyph.name not in features: glyphs_to_delete.add(glyph.name)
-for glyph_name in glyphs_to_delete:
-    del ufo[glyph_name]
-
-print('writing logs')
+glyph_labels_to_include.update(glyph_labels_to_include2)
+"""
 
 with open(log_dir + 'missing_glyphs.txt',"w",encoding="utf8") as f:
     f.write('\n'.join(sorted(missing_glyphs)))
@@ -197,12 +188,7 @@ with open(log_dir + 'chars_covered.txt',"w",encoding="utf8") as f:
 with open(log_dir + 'chars_displayed.txt',"w",encoding="utf8") as f:
     f.write('\n'.join(sorted(all_displayed_chars)))
 
-print('compiling ufo to ttf/otf')
-font = compileTTF(ufo)
-font.save(font_output)
-
-'''
-print('font saved, deleting excessive glyphs')
+font.save(font_output); print('font saved, deleting excessive glyphs')
 
 font = ttLib.TTFont(font_output)
 glyph_labels_to_include = set()
@@ -215,6 +201,5 @@ with open(glyphs_to_include_file_name,"w",encoding="utf8") as f:
     f.write(','.join(sorted(glyph_labels_to_include)))
 
 subprocess.run('pyftsubset ' + font_output + ' --output-file=' + font_output + ' --glyphs-file=' + glyphs_to_include_file_name, shell=True) # may not work, try changing the format of the input file 
-'''
-print('font saved')
+shutil.rmtree(font_combined_path)
 print('done')
